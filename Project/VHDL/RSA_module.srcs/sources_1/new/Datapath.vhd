efa43c7ccc74e0fcecdbb38557ef8e65a2a0cfb5
Torbjørn Viem Ness <tbness@gmail.com>
2016-11-20 14:02:43 +0100

    Created case statements for loading input data into registers, vivado keeps crashing

--- Project/VHDL/RSA_module.srcs/sources_1/new/Datapath.vhd
+++ Project/VHDL/RSA_module.srcs/sources_1/new/Datapath.vhd
@@ -97,15 +97,65 @@ begin
         message <= (others => '0');
         --operand_a <= (others => '0');
         --operand_b <= (others => '0');
-        key_n <= (others => '0');
-        key_n <= (others => '0');
-        key_n <= (others => '0');
-        key_n <= (others => '0');
-        key_n <= (others => '0');
+        message_chunk_ret <= (others => '0');
+        m_inverse <= (others => '0');
+        x_inverse <= (others => '0');
+        monpro_result <= (others => '0');
+        blakley_result <= (others => '0');
     elsif (clk'event AND clk = '1') then
+        --
+        case (load_msg) is
+        when "1000" =>
+            message((127-32*0) downto (32*3)) <= data_in;
+        when "0100" =>
+            message((127-32*1) downto (32*2)) <= data_in;
+        when "0010" =>
+            message((127-32*2) downto (32*1)) <= data_in;
+        when "0001" =>
+            message((127-32*3) downto (32*0)) <= data_in;
+        when others =>
+            message <= message;
+        end case;
+        case (load_key_n) is
+        when "1000" =>
+            key_n((127-32*0) downto (32*3)) <= data_in;
+        when "0100" =>
+            key_n((127-32*1) downto (32*2)) <= data_in;
+        when "0010" =>
+            key_n((127-32*2) downto (32*1)) <= data_in;
+        when "0001" =>
+            key_n((127-32*3) downto (32*0)) <= data_in;
+        when others =>
+            key_n <= key_n;
+        end case;
+        case (load_key_e) is
+        when "1000" =>
+            key_e((127-32*0) downto (32*3)) <= data_in;
+        when "0100" =>
+            key_e((127-32*1) downto (32*2)) <= data_in;
+        when "0010" =>
+            key_e((127-32*2) downto (32*1)) <= data_in;
+        when "0001" =>
+            key_e((127-32*3) downto (32*0)) <= data_in;
+        when others =>
+            key_e <= key_e;
+        end case;
+        case (output_result) is
+        when "1000" =>
+            data_out <= message_chunk_ret((127-32*0) downto (32*3));
+        when "0100" =>
+            data_out <= message_chunk_ret((127-32*1) downto (32*2));
+        when "0010" =>
+            data_out <= message_chunk_ret((127-32*2) downto (32*1));
+        when "0001" =>
+            data_out <= message_chunk_ret((127-32*3) downto (32*0));
+        when others =>
+            data_out <= (others => '0');
+        end case;
+
     end if;
 end process;
-    
+
 --arithmetics : entity work.ALU
 --        port map (
 --            operand_a => operand_a,
