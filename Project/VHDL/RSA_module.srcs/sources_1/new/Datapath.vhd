654edd553c396252749e6955d646d8f7896f8862
Torbjørn Viem Ness <tbness@gmail.com>
2016-11-25 23:49:10 +0100

    Removed commented code

--- Project/VHDL/RSA_module.srcs/sources_1/new/Datapath.vhd
+++ Project/VHDL/RSA_module.srcs/sources_1/new/Datapath.vhd
@@ -22,14 +22,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Datapath is
     -- TODO: make global (or at least up to the Datapath), currently also defined in ALU.vhd
@@ -309,16 +301,7 @@ reg_key_e3 : entity work.register_enable_reset_n
         q => key_e(127 downto 96)
     );
 
---reg_m_inverse : entity work.register_enable_reset_n
---    generic map(REGISTER_WIDTH => 128)
---    port map(
---        clk => clk,
---        reset_n => reset_n,
---        en => load_blakley_to_msg,
---        d => blakley_result,
---        q => m_inverse
---    );
-
+-- Working register
 reg_x_inverse : entity work.register_enable_reset_n
         generic map(REGISTER_WIDTH => 128)
         port map(
@@ -328,47 +311,4 @@ reg_x_inverse : entity work.register_enable_reset_n
             d => x_inverse_next,
             q => x_inverse
         );
-
---load_registers : process (clk, reset_n)
---begin
---    if (reset_n = '0') then -- Reset all registers to known default values
---        key_n <= (others => '0');
---        key_e <= (others => '0');
---        message_reg <= (others => '0');
---        --operand_a <= (others => '0');
---        --operand_b <= (others => '0');
---        message_chunk_ret <= (others => '0');
---        m_inverse <= (others => '0');
---        x_inverse <= (others => '0');
---        monpro_result <= (others => '0');
---        blakley_result <= (others => '0');
---    elsif (clk'event AND clk = '1') then
---        --Load the various registers based on the incoming control signals
---        case (select_output) is -- result chunk to output
---        when "1000" =>
---            data_out <= message_chunk_ret((127-32*0) downto (32*3));
---        when "0100" =>
---            data_out <= message_chunk_ret((127-32*1) downto (32*2));
---        when "0010" =>
---            data_out <= message_chunk_ret((127-32*2) downto (32*1));
---        when "0001" =>
---            data_out <= message_chunk_ret((127-32*3) downto (32*0));
---        when others =>
---            data_out <= (others => '0');
---        end case;
---        -- Load the result of blakley
---        if (load_blakley_to_msg = '1') then
---            m_inverse <= blakley_result;
---        end if; 
-
---    end if;
---end process;
-
---arithmetics : entity work.ALU
---        port map (
---            operand_a => operand_a,
---            operand_b => operand_b,
---            result => message_chunk_ret,
---            sub_sel => operation_sel
---        );
 end Behavioral;
